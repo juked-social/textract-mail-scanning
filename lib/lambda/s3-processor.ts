5e2732ad7293eeb4599b72e3ced9ad2b01d87369
michaelwalker <michaelhuytran@gmail.com>
2024-08-27 20:10:52 -0700

    s3 lambda attempt

--- lib/lambda/s3-processor.ts
+++ lib/lambda/s3-processor.ts
@@ -1,5 +1,11 @@
+import {S3Client} from '@aws-sdk/client-s3';
+
 export const handler = async (event: any) => {
     console.log(event);
+
+    const s3Client = new S3Client({ region: process.env.REGION });
+    const bucketName = process.env.IMAGE_BUCKET_NAME;
+
 
     const images = event?.images?.map((id: string) => ({
         id,
