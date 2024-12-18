063c6ed2fe88482a8eab73ab220504137f425562
Michael Walker <MichaelHuyWalker@gmail.com>
2024-12-18 16:47:43 +0200

    feat: update mail processing flow with Claude usage

--- lib/lambda/rotate-image.ts
+++ lib/lambda/rotate-image.ts
@@ -3,15 +3,26 @@ import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3
 import { Readable } from 'node:stream';
 import sharp from 'sharp';
 import { publishError } from './helpers';
+import { BedrockRuntimeClient } from '@aws-sdk/client-bedrock-runtime';
+import { getRotationDetectionPrompt, invokeBedrockModel } from './handler/bedrock-service';
 
+interface IRotationDetectionResult {
+    rotationRequired: boolean;
+    angleInDegrees: number;
+    confidence: number;
+    orientation: string;
+    detectionMethod: string;
+    recommendedAction: string;
+}
+
+const bedrockModelId = process.env.BEDROCK_MODEL_ID;
+const bedrockClient = new BedrockRuntimeClient({ region: process.env.REGION });
 const s3Client = new S3Client({ region: process.env.REGION });
 
-export async function processImage(imageBuffer: Buffer): Promise<Buffer> {
+export async function processImage(imageBuffer: Buffer, isRotationRequired: boolean, rotationAngle: number): Promise<Buffer> {
     let image = sharp(imageBuffer);
-    const metadata = await image.metadata();
-
-    if (metadata.height && metadata.width && metadata.height > metadata.width) {
-        image = image.rotate(-90);
+    if (isRotationRequired) {
+        image = image.rotate(-rotationAngle);
     }
 
     image
@@ -37,7 +48,11 @@ export const handler = async (event: any) => {
         if (!s3Path) {
             throw new Error('Missing required s3Path parameter');
         }
+        if (!bedrockModelId) {
+            throw new Error('Missing required bedrockModelId env variable');
+        }
         const { bucket, key } = splitS3Url(s3Path);
+        console.log(`Processing image: ${key}`);
 
         // Get image from S3
         const s3Command = new GetObjectCommand({
@@ -50,21 +65,29 @@ export const handler = async (event: any) => {
         if (!Body || !(Body instanceof Readable)) {
             throw new Error('Image not found or body is not a readable stream');
         }
-
-        // Convert stream to buffer
+        // validate image rotation
         const imageBuffer = await streamToBuffer(Body);
 
+        const imageBase64 = imageBuffer.toString('base64');
+        const prompt = getRotationDetectionPrompt();
+        const rotationDetectionResult = await invokeBedrockModel<IRotationDetectionResult>(
+            bedrockClient,
+            bedrockModelId,
+            imageBase64,
+            prompt,
+        );
+        console.log('rotationDetectionResult', rotationDetectionResult);
+        const { rotationRequired, angleInDegrees } = rotationDetectionResult;
         // Process image using sharp directly from the buffer
-        const processedBuffer = await processImage(imageBuffer);
-
+        const processedBuffer = await processImage(imageBuffer, rotationRequired, Number(angleInDegrees));
         // Upload processed image back to S3
         const s3ImagePut = new PutObjectCommand({
             Bucket: bucket,
             Key: key,
             Body: processedBuffer,
         });
-        await s3Client.send(s3ImagePut);
-
+        const putItemResult = await s3Client.send(s3ImagePut);
+        console.log('putItemResult', putItemResult);
     } catch (error) {
         console.error('Error during image processing:', error);
         await publishError('ImageRotator', error);
